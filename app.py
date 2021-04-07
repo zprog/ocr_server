47f57ed372919893d815753f4b9ed86bb52451ae
zap <zach@privette.net>
2021-04-07 17:51:03 -0400

    repo init from local

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask, render_template, request, url_for
+from werkzeug.datastructures import FileStorage
+#b64 decode strip mime
+import base64
+
+# import our OCR function
+#from ocr_core import ocr_core
+import ci2
+
+# define a folder to store and later serve the images
+UPLOAD_FOLDER = 'static/uploads/'
+
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# route and function to handle the home page
+#@app.route('/')
+#def home_page():
+    #return render_template('index.html')
+
+# route and function to handle the upload page
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if there is a file in the request
+        if 'b64_text_field' not in request.form:
+            return render_template('upload.html', msg='No file selected')
+        b64_string = str.encode(request.form['b64_text_field'].split(",")[1])
+        b64_name = request.form['b64_name']
+        #file = FileStorage(content_type='image/png', filename='newImg242.png', name='image_placeholder', content_length=0, stream=b64_string)
+        img_ready = "roiCrop_of_" + b64_name
+        with open("static/uploads/" + img_ready, "wb") as fh:
+            fh.write(base64.decodebytes(b64_string))
+        extracted_text = ci2.ocr(img_ready)
+        #extra white space is caused by \x0c
+        return render_template('upload.html', msg='Successfully processed', extracted_text=extracted_text, results="results", img_src=url_for('static', filename='uploads/' + img_ready), code=301)
+
+            #file = FileStorage(stream=fh, content_type='image/png', filename='newImg448.png',)
+        #file = object
+        #zachs debug
+        #print(file.filename)
+        ## if no file is selected
+        #if file.filename == '':
+        #    return render_template('upload.html', msg='No file selected')
+
+        #if file and allowed_file(file.filename):
+        #    file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], file.filename))
+
+            ## call the OCR function on it
+            #extracted_text = ocr_core(file)
+
+            # call ci2 function on it
+        #print("THIS IS THE FILE: " + file.filename)
+        #extracted_text = ci2.ocr(file.filename)
+
+
+        # extract the text and display it
+        #print(url_for('static', filename='uploads/' + file.filename, code=301))
+
+           # return render_template('upload.html', msg='Successfully processed', extracted_text=extracted_text, img_src=url_for('static', filename='uploads/' + file.filename), code=301)
+    elif request.method == 'GET':
+        return render_template('upload.html', crop="yes")
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ ci2.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from ocr_core import ocr_core
+from PIL import Image
+import pytesseract
+
+def ocr_init():
+    #ORIGINAL_IMAGE = "IMG_4031.jpg"
+    ORIGINAL_IMAGE = ""
+    #image = cv2.imread("images/" + ORIGINAL_IMAGE)
+
+def ocr(ORIGINAL_IMAGE):
+    image = cv2.imread("static/uploads/" + ORIGINAL_IMAGE)
+    roi = image
+    #convert to grey
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    #threshold image
+    thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
+    print("TRESH: " + pytesseract.image_to_string(thresh))
+
+    # apply morphology to clean up small white or black regions
+    kernel = np.ones((5,5), np.uint8)
+    morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    morph = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel)
+
+    # thin region to remove excess black border
+    kernel = np.ones((3,3), np.uint8)
+    morph = cv2.morphologyEx(morph, cv2.MORPH_ERODE, kernel)
+
+    print("MORPH: " + pytesseract.image_to_string(morph))
+
+    img = roi
+
+    # applying different thresholding
+    # techniques on the input image
+    # all pixels value above 120 will
+    # be set to 255
+    ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY_INV)
+    ret, thresh3 = cv2.threshold(img, 120, 255, cv2.THRESH_TRUNC)
+    ret, thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
+    ret, thresh5 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO_INV)
+
+    # the window showing output images
+    # with the corresponding thresholding
+    # techniques applied to the input images
+#    cv2.imshow('Binary Threshold', thresh1)
+#    cv2.imshow('Binary Threshold Inverted', thresh2)
+#    cv2.imshow('Truncated Threshold', thresh3)
+#    cv2.imshow('Set to 0', thresh4)
+#    cv2.imshow('Set to 0 Inverted', thresh5)
+
+    print("t1: " + pytesseract.image_to_string(thresh1))
+    print("t2: " + pytesseract.image_to_string(thresh2))
+    print("t3: " + pytesseract.image_to_string(thresh3))
+    print("t4: " + pytesseract.image_to_string(thresh4))
+    print("t5: " + pytesseract.image_to_string(thresh5))
+    thresh_dict = {
+            'Origin': pytesseract.image_to_string(roi),
+            'M0': pytesseract.image_to_string(morph),
+            'T0': pytesseract.image_to_string(thresh),
+            'T1': pytesseract.image_to_string(thresh1),
+            'T2': pytesseract.image_to_string(thresh2),
+            'T3': pytesseract.image_to_string(thresh3),
+            'T4': pytesseract.image_to_string(thresh4),
+            'T5': pytesseract.image_to_string(thresh5)
+            }
+
+    print(pytesseract.image_to_string(roi))
+    return thresh_dict
+
