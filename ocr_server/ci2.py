e48acc851262abe24c91fbef6e0a24806cb83948
zap <zach@privette.net>
2021-04-10 09:47:03 -0400

    Removing debug/print statements

--- ocr_server/ci2.py
+++ ocr_server/ci2.py
@@ -1,18 +1,10 @@
 import cv2
 import numpy as np
-#from . import ocr_core
 from PIL import Image
 import pytesseract
-#b64 decode strip mime
 import base64
 
-def ocr_init():
-    #ORIGINAL_IMAGE = "IMG_4031.jpg"
-    ORIGINAL_IMAGE = ""
-    #image = cv2.imread("images/" + ORIGINAL_IMAGE)
-
 def ocr(ORIGINAL_IMAGE):
-    # image = cv2.imread("static/uploads/" + ORIGINAL_IMAGE)
     nparr = np.frombuffer(base64.decodebytes(ORIGINAL_IMAGE), dtype="uint8")
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     roi = image
@@ -20,7 +12,6 @@ def ocr(ORIGINAL_IMAGE):
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     #threshold image
     thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
-    #print("TRESH: " + pytesseract.image_to_string(thresh))
 
     # apply morphology to clean up small white or black regions
     kernel = np.ones((5,5), np.uint8)
@@ -30,20 +21,12 @@ def ocr(ORIGINAL_IMAGE):
     # thin region to remove excess black border
     kernel = np.ones((3,3), np.uint8)
     morph = cv2.morphologyEx(morph, cv2.MORPH_ERODE, kernel)
-    #print("MORPH: " + pytesseract.image_to_string(morph))
 
     img = roi
 
-    # applying different thresholding
-    # techniques on the input image
-    # all pixels value above 120 will
-    # be set to 255
+    # applying different thresholding techniques on the input image
+    # all pixels value above 120 will be set to 255
     ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
-    # ret, thresh2 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY_INV)
-    # ret, thresh3 = cv2.threshold(img, 120, 255, cv2.THRESH_TRUNC)
-    # ret, thresh4 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
-    # ret, thresh5 = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO_INV)
-
 
     # the first two: morph and thresh seem to be the best over all.
     # let's use those too and see how it affects performance.
@@ -53,17 +36,5 @@ def ocr(ORIGINAL_IMAGE):
             'T0': pytesseract.image_to_string(thresh),
             }
 
-    # thresh_dict = {
-    #         'Origin': pytesseract.image_to_string(roi),
-    #         'M0': pytesseract.image_to_string(morph),
-    #         'T0': pytesseract.image_to_string(thresh),
-    #         'T1': pytesseract.image_to_string(thresh1),
-    #         'T2': pytesseract.image_to_string(thresh2),
-    #         'T3': pytesseract.image_to_string(thresh3),
-    #         'T4': pytesseract.image_to_string(thresh4),
-    #         'T5': pytesseract.image_to_string(thresh5)
-    #         }
-
-    #print(pytesseract.image_to_string(roi))
     return thresh_dict
 
